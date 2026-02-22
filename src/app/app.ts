fc325e029a7b5793f754fc58d32fc4b2bb08f22a
farzin mossaed <fmossaed@gmail.com>
2026-02-22 12:09:49 +0330

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,44 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, inject, OnInit, signal, WritableSignal} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {JsonPipe} from '@angular/common';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [JsonPipe, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('angular-v-21');
+  http = inject(HttpClient);
+  data: WritableSignal<any> = signal([]);
+  email: string = ''
+  otp: string = ''
+
+  ngOnInit() {
+  }
+
+  signIn() {
+    this.http.post('http://localhost:3000/auth/signin-otp', {email: this.email}).subscribe(res => {
+      console.log(res)
+    })
+  }
+
+  verifyOTP(){
+    this.http.post('http://localhost:3000/auth/verify-signin-otp', {email: this.email, otp: this.otp}).subscribe(res => {
+      console.log(res)
+      localStorage.setItem('verify-otp-res', JSON.stringify(res))
+    })
+  }
+
+  getTasks() {
+    this.http.get('http://localhost:3000/api/tasks', {withCredentials: true}).subscribe({
+      next: (res: any) => {
+        this.data.set(res)
+      }, error: () => {
+        this.data.set([])
+      }
+    })
+  }
 }
---
+++ src/app/core/interceptors/auth-interceptor.ts
@@ -0,0 +1,10 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+
+  const authReq = req.clone({
+    withCredentials: true   // send cookies automatically
+  });
+
+  return next(authReq);
+};
